d77272407176818e6a32a5c1a387e221fe27a5ca
Alejandro A <alejandro.agustin@est.fib.upc.edu>
2017-01-03 23:01:27 +0100

    RecyclerView

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/BookAdapter.java
@@ -0,0 +1,110 @@
+package com.example.pr_idi.mydatabaseexample;
+
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    private static final String TAG = BookAdapter.class.getSimpleName();
+
+    private List<Book> bookList;
+    private String aux;
+
+
+    public BookAdapter(List<Book> bookList, String aux) {
+        this.bookList = bookList;
+        this.aux = aux;
+    }
+
+    /**
+     *
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param parent The ViewGroup that these ViewHolders are contained within.
+     * @param viewType  If your RecyclerView has more than one type of item (which ours doesn't) you
+     *                  can use this viewType integer to provide a different layout. See
+     *                  {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                  for more details.
+     * @return A new NumberViewHolder that holds the View for each list item
+     */
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        BookViewHolder viewHolder = new BookViewHolder(view);
+
+        return viewHolder;
+    }
+
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the correct
+     * indices in the list for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        Book book = bookList.get(position);
+        holder.title.setText(book.getTitle());
+        if (aux.equals("author")){
+            holder.genre.setText(book.getAuthor());
+        }
+        if (book.getYear() != -1) {
+            holder.year.setText(String.valueOf(book.getYear()));
+        }
+        Log.d(TAG, book.getTitle());
+    }
+
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our forecast
+     */
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    /**
+     * Cache of the children views for a list item.
+     */
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title, year, genre;
+
+
+        /**
+         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
+         * TextViews and set an onClickListener to listen for clicks. Those will be handled in the
+         * onClick method below.
+         */
+        public BookViewHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.title);
+            year  = (TextView) itemView.findViewById(R.id.year );
+            genre = (TextView) itemView.findViewById(R.id.genre);
+        }
+    }
+
+}
--- app/src/main/java/com/example/pr_idi/mydatabaseexample/BookData.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/BookData.java
@@ -9,12 +9,17 @@ import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 public class BookData {
+
+    private final static String unknown = "Unknown";
+    public static final String PREFS_NAME = "MyPrefsFile";
+
 
     // Database fields
     private SQLiteDatabase database;
@@ -24,7 +29,7 @@ public class BookData {
 
     // Here we only select Title and Author, must select the appropriate columns
     private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
-            MySQLiteHelper.COLUMN_TITLE, MySQLiteHelper.COLUMN_AUTHOR};
+            MySQLiteHelper.COLUMN_TITLE, MySQLiteHelper.COLUMN_AUTHOR, MySQLiteHelper.COLUMN_CATEGORY, MySQLiteHelper.COLUMN_YEAR, MySQLiteHelper.COLUMN_PERSONAL_EVALUATION};
 
     public BookData(Context context) {
         dbHelper = new MySQLiteHelper(context);
@@ -32,26 +37,29 @@ public class BookData {
 
     public void open() throws SQLException {
         database = dbHelper.getWritableDatabase();
+
     }
 
     public void close() {
         dbHelper.close();
     }
 
-    public Book createBook(String title, String author) {
+    //String or null
+    public Book createBook(String title, String author, String category, String year, String eval) {
         ContentValues values = new ContentValues();
         Log.d("Creating", "Creating " + title + " " + author);
 
         // Add data: Note that this method only provides title and author
         // Must modify the method to add the full data
         values.put(MySQLiteHelper.COLUMN_TITLE, title);
-        values.put(MySQLiteHelper.COLUMN_AUTHOR, author);
+
+        values.put(MySQLiteHelper.COLUMN_AUTHOR, (author != null) ? author : unknown);
 
         // Invented data
-        values.put(MySQLiteHelper.COLUMN_PUBLISHER, "Do not know");
-        values.put(MySQLiteHelper.COLUMN_YEAR, 2030);
-        values.put(MySQLiteHelper.COLUMN_CATEGORY, "Fantasia");
-        values.put(MySQLiteHelper.COLUMN_PERSONAL_EVALUATION, "regular");
+        values.put(MySQLiteHelper.COLUMN_PUBLISHER, unknown);
+        values.put(MySQLiteHelper.COLUMN_YEAR, (year != null) ? year : "-1");
+        values.put(MySQLiteHelper.COLUMN_CATEGORY, (category != null) ? category : unknown);
+        values.put(MySQLiteHelper.COLUMN_PERSONAL_EVALUATION, (eval != null) ? eval : unknown);
 
         // Actual insertion of the data using the values variable
         long insertId = database.insert(MySQLiteHelper.TABLE_BOOKS, null,
@@ -105,6 +113,9 @@ public class BookData {
         book.setId(cursor.getLong(0));
         book.setTitle(cursor.getString(1));
         book.setAuthor(cursor.getString(2));
-        return book;
+        book.setCategory(cursor.getString(3));
+        book.setYear( Integer.parseInt(cursor.getString(4)));
+        book.setPersonal_evaluation(cursor.getString(5));
+    return book;
     }
 }
